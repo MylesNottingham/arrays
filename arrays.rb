4039803b3448db224d0033a0d931e8ee9898d02b
Myles Nottingham <MylesNottingham@gmail.com>
2023-02-22 15:32:10 -0700

    Add four array methods and their explanations

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,16 @@
 fruits = ["apple", "banana", "peach", "pear"]
 primes = [2, 3, 5, 7]
-floats = [1.2, 3.4, 5.6, 7.8]
+floats = [1.2, 2.3, 3.4, 4.5]
 bools = [true, false, true, false]
+
+# Removes removes last element from array
+fruits.pop
+
+# Removes first element from array
+primes.shift
+
+# Appends element to the end of array
+floats.push 5.6
+
+# Prepends element to the beginning of array
+bools.unshift false
