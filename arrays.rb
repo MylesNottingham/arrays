0672908d87a448ce48f847e2debcb80167c887b6
Myles Nottingham <MylesNottingham@gmail.com>
2023-02-22 15:46:22 -0700

    Add two puts and a description to showcase array index comprehension

--- arrays.rb
+++ arrays.rb
@@ -14,3 +14,7 @@ floats.push 5.6
 
 # Prepends element to the beginning of array
 bools.unshift false
+
+# Uses elements at specific indexes from arrays to complete sentences.
+puts "It is #{bools[1]} that the best #{fruits[1]} is bright yellow with light freckles."
+puts "I put at least #{primes[0]} different kinds of fruit in my smoothie and blend for intervals of #{floats[4]} seconds."
